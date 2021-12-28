128ebc36b3ce8b386f87b0b49c9044c8a412cbeb
Ben <bzilber@users.noreply.github.com>
2021-12-28 18:25:38 -0500

    name added

--- myScript.js
+++ myScript.js
@@ -1,3 +1,13 @@
+var student = document.querySelector('#name');
+
 fetch('https://api.hatchways.io/assessment/students')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        //student = data[0]['firstName'];
+        var nameValue = data['students'][0]['firstName'];
+
+        //console.log(student);
+
+        student.innerHTML = nameValue;
+
+    })
